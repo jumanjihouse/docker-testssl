a0cf853f8c25f7fe09cc6e7ed6084eccc1fc1296
Paul Morgan <jumanjiman@gmail.com>
2015-09-17 21:53:42 -0400

    publish images to quay

---
+++ publish.sh
@@ -0,0 +1,42 @@
+#!/bin/bash
+# vim: set sw=2 ts=2 ai et:
+set -e
+
+if [[ $# -lt 1 ]]; then
+  echo "ERR: you must pass 1 or more tags as arguments" >&2
+  exit 1
+fi
+
+suffixes=$@
+
+# The names of the images as built.
+build_tags="
+testssl:master
+testssl:2.6
+"
+
+smitty() {
+  echo "[INFO] $@"
+  eval $@
+}
+
+docker login -e ${mail} -u ${user} -p ${pass} quay.io
+
+for build_tag in ${build_tags}; do
+  echo "===== ${build_tag} ====="
+  for suffix in ${suffixes}; do
+    echo "-- ${suffix}"
+
+    new_name="quay.io/jumanjiman/${build_tag}-${suffix}"
+
+    # Create a new tag.
+    smitty docker tag -f ${build_tag} ${new_name}
+
+    # Publish the tagged image.
+    smitty docker push ${new_name}
+    echo
+    echo
+  done
+done
+
+docker logout quay.io
