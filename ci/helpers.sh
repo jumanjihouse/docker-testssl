dc51ba6f425127fa3353daecc84ba2de0ef87e90
Paul Morgan <jumanjiman@gmail.com>
2017-11-18 17:39:09 +0000

    ci: refactor scripts to improve shell style

--- ci/helpers.sh
+++ ci/helpers.sh
@@ -5,30 +5,41 @@ verbosity=1
 
 # Helper functions.
 err() {
-  echo ERROR: $* >&2
+  echo ERROR: "$*" >&2
 }
 
 info() {
   if [[ ${verbosity} -ge 1 ]]; then
-    echo INFO: $* >&2
+    echo INFO: "$*" >&2
   fi
 }
 
 debug() {
   if [[ ${verbosity} -ge 2 ]]; then
-    echo DEBUG: $* >&2
+    echo DEBUG: "$*" >&2
   fi
 }
 
 finish() {
-  _code=$?
+  local _code=$?
+  readonly _code
 
   if [ ${_code} -eq 0 ]; then
-    info $0 OK
+    info "$0" OK
   else
-    err $0 failed with exit code ${_code}
+    err "$0" failed with exit code ${_code}
     exit ${_code}
   fi
+}
+
+handle_err() {
+  local RC=$?
+
+  # $BASH_COMMAND contains the command that was being executed at the time of the trap
+  # ${BASH_LINENO[0]} contains the line number in the script of that command
+  err "${RC} from \"${BASH_COMMAND}\" on line ${BASH_LINENO[0]}"
+
+  exit ${RC}
 }
 
 is_ci() {
@@ -37,4 +48,7 @@ is_ci() {
 }
 
 # Traps.
+# NOTE: In POSIX, beside signals, only EXIT is valid as an event.
+#       You must use bash to use ERR.
 trap finish EXIT
+trap handle_err ERR
